c92cff7729fed94e1c755060f1f0f08fa087c387
Tim <timcpb@hotmail.com>
2020-10-20 18:26:44 +0100

    green test - removes the content of the parenthesis

--- spec/RemoveParenthesisSpec.js
+++ spec/RemoveParenthesisSpec.js
@@ -2,7 +2,7 @@ describe("removeParentheses", function(){
     it("returns a string without parenthesis", function() {
         expect(removeParentheses("Hello()")).toEqual("Hello")
     })
-    it("also removes the content of the parenthesis", function() {
+    it("removes the content of the parenthesis", function() {
         expect(removeParentheses("Hello(world)")).toEqual("Hello")
     })
 })
--- src/RemoveParenthesis.js
+++ src/RemoveParenthesis.js
@@ -1,10 +1,16 @@
 function removeParentheses(s){
-    array = s.split("")
-    newArray = []
-    array.forEach(x => {
-        if(x !== "(" && x !== ")") {
-            newArray.push(x)
-        }
-    });
-    return newArray.join('')
+    // array = s.split("")
+    // newArray = []
+    // array.forEach(x => {
+    //     if(x !== "(" && x !== ")") {
+    //         newArray.push(x)
+    //     }
+    // });
+    // return newArray.join('')
+    var a = s.indexOf("(")
+    var b = s.indexOf(")")
+    var parenthesesContent = s.substr(a, b)
+    var result = s.replace(parenthesesContent, "")
+    return result
+    
 }
