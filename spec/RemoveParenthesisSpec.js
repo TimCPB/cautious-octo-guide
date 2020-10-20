846a897dd05daa682a3fb714f0b090138b0c12df
Tim <timcpb@hotmail.com>
2020-10-20 18:46:43 +0100

    red test - removes the content from multiple nested parentheses

--- spec/RemoveParenthesisSpec.js
+++ spec/RemoveParenthesisSpec.js
@@ -5,7 +5,10 @@ describe("removeParentheses", function(){
     it("removes the content of the parentheses", function() {
         expect(removeParentheses("Hello(world)")).toEqual("Hello")
     })
-    it("removes the content the parentheses but leaves content ", function() {
+    it("removes the content the parentheses but leaves content before and after", function() {
         expect(removeParentheses("a (bc d)e")).toEqual("a e")
+    })
+    it("removes the content from multiple nested parentheses", function() {
+        expect(removeParentheses("a(b(c))")).toEqual("a")
     })
 })
