0c5794319c4ca49153bee379fcb7f32f51992c3b
Tim <timcpb@hotmail.com>
2020-10-20 15:12:43 +0100

    red test - returns 15 from [4,5,7,5,4,8]

--- spec/RepeatsSpec.js
+++ spec/RepeatsSpec.js
@@ -2,4 +2,8 @@ describe("repeats function", function() {
     it("returns sum of two elements", function() {
         expect(repeats([1,2])).toEqual(3)
     });
+
+    it("returns 15 from [4,5,7,5,4,8]", function() {
+        expect(repeats([4,5,7,5,4,8])).toEqual(15)
+    });
 });
