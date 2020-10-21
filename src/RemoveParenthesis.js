5ab9bce87bcdccd4066b88e2ca2efe99fc12c90e
Tim <timcpb@hotmail.com>
2020-10-22 00:52:11 +0100

    green test - removes the content from multiple separate parentheses

--- src/RemoveParenthesis.js
+++ src/RemoveParenthesis.js
@@ -9,10 +9,24 @@ function removeParentheses(s){
     // return newArray.join('')
     var a = s.indexOf("(")
     var b = s.lastIndexOf(")")
-    var parenthesesContent = s.substring(a, b + 1)
-    console.log(parenthesesContent)
-    console.log(a, b)
-    var result = s.replace(parenthesesContent, "")
-    return result
+    var c = s.indexOf("(", a + 1)
+    var d = s.indexOf(")")
+    if (c > d) {
+        var newString = s
+        while (newString.indexOf("(") !== -1) {
+            var f = newString.indexOf("(")
+            var g = newString.indexOf(")")
+            var content = newString.substring(f, g + 1)
+            newString = newString.replace(content, "")
+        }
+        return newString
+    } 
+    else {
+        var parenthesesContent = s.substring(a, b + 1)
+        console.log(parenthesesContent)
+        console.log(a, b)
+        var result = s.replace(parenthesesContent, "")
+        return result
+    }
     
 }
