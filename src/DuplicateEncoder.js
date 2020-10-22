7c8fc528d9833c7244e3f6a20ef6f9fbe0356acf
Tim <timcpb@hotmail.com>
2020-10-22 02:18:18 +0100

    replaces recurring letters with ')'

--- src/DuplicateEncoder.js
+++ src/DuplicateEncoder.js
@@ -2,7 +2,11 @@ function duplicateEncode(word) {
     let array = word.split("")
     let newArray = []
     array.forEach(x => {
-        newArray.push("(")
+        if (word.indexOf(x) === word.lastIndexOf(x)) {
+            newArray.push("(")
+        } else {
+            newArray.push(")")
+        }
     })
     return newArray.join("")
 }
