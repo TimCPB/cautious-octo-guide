3e8efc4f030673eda95e3c230cf762d027b9713a
Tim <timcpb@hotmail.com>
2020-10-22 02:03:39 +0100

    red test - replaces recurring letters with ')'

--- spec/DuplicateEncoderSpec.js
+++ spec/DuplicateEncoderSpec.js
@@ -2,4 +2,7 @@ describe("duplicateEncoder", function(){
     it("replaces single instances of letters with '('", function() {
         expect(duplicateEncode("din")).toEqual("(((")
     })
+    it("replaces recurring letters with ')'", function() {
+        expect(duplicateEncode("recede")).toEqual("()()()")
+    })
 })
