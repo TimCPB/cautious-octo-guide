620017e3ca0136810727484deb10e3d2de855d93
Tim <timcpb@hotmail.com>
2020-10-22 01:58:47 +0100

    green test - replaces single instances of letters with '('

--- src/DuplicateEncoder.js
+++ src/DuplicateEncoder.js
@@ -1,3 +1,8 @@
 function duplicateEncode(word) {
-    return "hello"
+    let array = word.split("")
+    let newArray = []
+    array.forEach(x => {
+        newArray.push("(")
+    })
+    return newArray.join("")
 }
