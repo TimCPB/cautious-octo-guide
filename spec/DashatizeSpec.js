480dc85f7f55ade3cfa05a2f0f87b4b679376993
Tim <timcpb@hotmail.com>
2020-10-26 23:20:26 +0000

    red test - inserts a dash before and after an odd integer

--- spec/DashatizeSpec.js
+++ spec/DashatizeSpec.js
@@ -2,4 +2,7 @@ describe("Dashatize", function() {
     it("inserts a dash into a 2 digit number containting one integer", function(){
         expect(dashatize(27)).toEqual("2-7")
     })
+    it("inserts a dash before and after an odd integer", function(){
+        expect(dashatize(274)).toEqual("2-7-4")
+    })
 })
