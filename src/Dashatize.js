498bbbdf434af5d13a6d2b3c1adaee61b5207c46
Tim <timcpb@hotmail.com>
2020-10-26 23:31:29 +0000

    green test - inserts a dash before and after an odd integer

--- src/Dashatize.js
+++ src/Dashatize.js
@@ -1,9 +1,12 @@
 function dashatize(num) {
     var initialString = num.toString()
     var initialArray = initialString.split("")
+    var initialArrayLength = initialArray.length
     var newArray = []
-    initialArray.forEach(x => {
-        if(x % 2 !== 0){
+    initialArray.forEach((x, i) => {
+        if(x % 2 !== 0 && i !== initialArrayLength - 1) {
+            newArray.push(`-${x}-`)
+        } else if(x % 2 !== 0 && i === initialArrayLength - 1) {
             newArray.push(`-${x}`)
         } else {
             newArray.push(x)
