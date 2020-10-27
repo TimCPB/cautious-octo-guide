dc24b2ea77b42495e88ee51efea306ed674d995a
Tim <timcpb@hotmail.com>
2020-10-27 21:24:49 +0000

    green test - treats negative numbers as if they were positive

--- src/Dashatize.js
+++ src/Dashatize.js
@@ -1,16 +1,14 @@
 function dashatize(num) {
-    var initialString = num.toString()
+    var initialString = Math.abs(num).toString()
     var initialArray = initialString.split("")
-    console.log(initialArray)
     var initialArrayLength = initialArray.length
     var newArray = []
     initialArray.forEach((x, i) => {
-        console.log(x, i)
         if(x % 2 !== 0 && i !== initialArrayLength - 1 && i !== 0) {
             newArray.push(`-${x}-`)
-        } else if(x % 2 !== 0 && i === initialArrayLength - 1) {
+        } else if(x % 2 !== 0 && i === initialArrayLength - 1 && i !== 0) {
             newArray.push(`-${x}`)
-        } else if(x % 2 !== 0 && i === 0) {
+        } else if(x % 2 !== 0 && i === 0 && i !== initialArrayLength - 1) {
             newArray.push(`${x}-`)
         } else {
             newArray.push(x)
