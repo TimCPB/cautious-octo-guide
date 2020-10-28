0a6d33bf93e2e5834ea9a2f0171f08ce63c83350
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-10-28 18:14:26 +0000

    extra test for lower case letter, and silly test at end

--- spec/MumblingSpec.js
+++ spec/MumblingSpec.js
@@ -3,6 +3,9 @@ describe("accum", function() {
   it ("returns 'A' when given 'A'", function() {
     expect(accum("A")).toEqual("A")
   });
+  it ("returns 'A' when given 'a'", function() {
+    expect(accum("a")).toEqual("A")
+  })
   it ("returns 'B' when given 'B'", function() {
     expect(accum("B")).toEqual("B")
   })
@@ -12,4 +15,10 @@ describe("accum", function() {
   it ("returns 'A-Bb' when given 'AB'", function(){
     expect(accum("AB")).toEqual("A-Bb")
   })
+  it ("returns 'H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc' when given 'HbideVbxncC'", function(){
+    expect(accum("HbideVbxncC")).toEqual("H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc")
+  })
+
+
+
 })
