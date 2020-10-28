4f80681891cea0fc8121253eae5f289cdab23c7e
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-10-28 18:08:18 +0000

    green test - multiple letters given

--- spec/MumblingSpec.js
+++ spec/MumblingSpec.js
@@ -1,14 +1,14 @@
 describe("accum", function() {
 
-//   it ("returns 'A' when given 'A'", function() {
-//     expect(accum("A")).toEqual("A")
-//   });
-//   it ("returns 'B' when given 'B'", function() {
-//     expect(accum("B")).toEqual("B")
-//   })
-//   it ("returns 'A-Aa' when given 'AA'", function(){
-//     expect(accum("AA")).toEqual("A-Aa")
-//   })
+  it ("returns 'A' when given 'A'", function() {
+    expect(accum("A")).toEqual("A")
+  });
+  it ("returns 'B' when given 'B'", function() {
+    expect(accum("B")).toEqual("B")
+  })
+  it ("returns 'A-Aa' when given 'AA'", function(){
+    expect(accum("AA")).toEqual("A-Aa")
+  })
   it ("returns 'A-Bb' when given 'AB'", function(){
     expect(accum("AB")).toEqual("A-Bb")
   })
--- src/Mumbling.js
+++ src/Mumbling.js
@@ -1,19 +1,21 @@
 function accum(s) {
   const wordArray = s.split("")
   let newWord = []
-  console.log(wordArray)
+  let newNewWord = []
 
     wordArray.forEach((letter, index) => {
         console.log(letter, index)
-      // input 
-      //push letter x (index + 1)
-      // var newElement = 
       let newElement = []
       for(let i = 0; i < (index + 1); i++) {
           newElement.push(letter.toLowerCase())
       }
       newWord.push(newElement.join(""))
     })
-    console.log(newWord)
-  return newWord.join("-")
+
+    newWord.forEach(word => {
+      let wordCapitalized = word.charAt(0).toUpperCase() + word.slice(1)
+     newNewWord.push(wordCapitalized)
+    })
+
+  return newNewWord.join("-")
 }
