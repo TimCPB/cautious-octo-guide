c484ed970223c57990d318764ecb7ba8baec7223
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-10-28 17:13:08 +0000

    returns B when given B

--- spec/MumblingSpec.js
+++ spec/MumblingSpec.js
@@ -3,4 +3,7 @@ describe("accum", function() {
   it ("returns 'A' when given 'A'", function() {
     expect(accum("A")).toEqual("A")
   });
+  it ("returns 'B' when given 'B'", function() {
+    expect(accum("B")).toEqual("B")
+  })
 })
