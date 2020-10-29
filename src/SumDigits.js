e43ff011a41fac7cafa04e7a06ffc5b7b495e607
Tim <timcpb@hotmail.com>
2020-10-29 22:00:46 +0000

    red test - returns the sum of two digits

--- src/SumDigits.js
+++ src/SumDigits.js
@@ -3,4 +3,9 @@ describe ("SumDigits", function() {
     it ("returns a single digit", function() {
         expect(sumDigits(4)).toEqual(4)
     })
+
+    it ("returns the sum of two digits", function() {
+        expect(sumDigits(10)).toEqual(1)
+        expect(sumDigits(99)).toEqual(18)
+    })
 })
