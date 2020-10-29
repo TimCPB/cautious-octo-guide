b7366de0dd63e3e840448c401385b456f1feed66
Tim <timcpb@hotmail.com>
2020-10-29 22:16:09 +0000

    refactor with Math.abs()

--- spec/SumDigitsSpec.js
+++ spec/SumDigitsSpec.js
@@ -1,11 +1,8 @@
 function sumDigits(num) {
-    var array = Array.from(String(num), Number)
-    console.log(array)
+    var array = Array.from(String(Math.abs(num)), Number)
     var sum = 0
     array.forEach(digit => {
-        if (!isNaN(digit)) {
             sum += digit
-        }
-    })
+        })
     return sum
 }
