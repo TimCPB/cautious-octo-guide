07a631e12cf667560521d24c6932fd163a7cb21f
Tim <timcpb@hotmail.com>
2020-11-03 15:37:40 +0000

    red test - explains that two people have liked a post

--- spec/LikesSpec.js
+++ spec/LikesSpec.js
@@ -7,4 +7,8 @@ describe ("Likes", function() {
     it("explains that one person has liked a post", function() {
         expect(likes(['Peter'])).toEqual('Peter likes this')
     })
+    
+    it("explains that two people have liked a post", function() {
+        expect(likes(['Jacob', 'Alex'])).toEqual('Jacob and Alex like this')
+    })
 })
