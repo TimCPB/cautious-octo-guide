b1aa614b77e978baef265c850cb2bdd07b73d30e
Katie McDonagh <mcdonagh.katie@outlook.com>
2020-11-03 16:15:29 +0000

    red test -returns the same integer when given 3 integers

--- spec/findTheOddIntegerSpec.js
+++ spec/findTheOddIntegerSpec.js
@@ -2,4 +2,8 @@ describe ("oddInteger", function(){
   it ("returns an the same integer when given one", function(){
     expect(oddInteger([10])).toEqual(10)
   })
+
+  it ("returns the odd integer when given 3 integers", function(){
+    expect(oddInteger([1,2,2])).toEqual(1)
+  })
 })
