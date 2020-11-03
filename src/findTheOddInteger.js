699ce6a7fcb9be302efaf1b80eb0f2a0a3d88684
Tim <timcpb@hotmail.com>
2020-11-03 16:58:07 +0000

    refactor

--- src/findTheOddInteger.js
+++ src/findTheOddInteger.js
@@ -2,8 +2,8 @@ function oddInteger(A){
   let oddInteger = ''
   A.forEach(integer => {
     let count = 0
-    A.forEach(number => {
-      if(number === integer) count += 1
+    A.forEach(element => {
+      if(element === integer) count += 1
     })
     if (count % 2 !== 0) oddInteger = integer
   })
