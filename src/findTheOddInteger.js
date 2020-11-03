02c20d63d33e558edda0f13102eb1d7026356da1
Tim <timcpb@hotmail.com>
2020-11-03 16:45:20 +0000

    green test - returns the odd integer when given 3 integers

--- src/findTheOddInteger.js
+++ src/findTheOddInteger.js
@@ -1,13 +1,17 @@
 function oddInteger(array){
-  
+  let oddInteger = ''
   array.forEach(integer => {
     let value = integer
     let count = 0
     array.forEach(number => {
       if(number === value){
         count += 1
+        console.log(`count for ${number} - ${count}`)
       }
     })
+    if (count === 1) {
+      oddInteger = integer
+    }
   })
-  return count
+  return oddInteger
 }
