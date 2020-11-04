23ff260539675e1ce0f1a9d20b4c47ef5494aa95
Tim <timcpb@hotmail.com>
2020-11-04 13:46:50 +0000

    red test - capitalizes the first letter of every sentence

--- spec/FixSpec.js
+++ spec/FixSpec.js
@@ -8,4 +8,10 @@ describe ("Fix", function(){
         expect(fix("hi.")).toEqual("Hi.");
     })
 
+    it("capitalizes the first letter of every sentence", function(){
+        const unFixedParagraph = "hello. my name is inigo montoya. you killed my father. prepare to die."
+        const fixedParagraph = "Hello. My name is inigo montoya. You killed my father. Prepare to die."
+        expect(fix(unFixedParagraph)).toEqual(fixedParagraph);
+    })
+
 })
