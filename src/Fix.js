3143472c757a6acdc6c148d953a9950da95238b4
Tim <timcpb@hotmail.com>
2020-11-04 13:44:22 +0000

    green test - capitalizes the first letter of a paragraph

--- src/Fix.js
+++ src/Fix.js
@@ -1,3 +1,8 @@
 function fix(paragraph){
-    return paragraph
+    let array = paragraph.split("")
+    let newArray = array.map((character, i) => {
+        if(i === 0)  return character.toUpperCase()
+        else return character
+    })
+    return newArray.join("")
 }
