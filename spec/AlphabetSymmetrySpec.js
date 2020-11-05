79bae6408ff6421a0c6b825fd55aa89a8f0ab6b5
Tim <timcpb@hotmail.com>
2020-11-05 13:33:13 +0000

    red test - solves for a single word containing upper and lower case letters

--- spec/AlphabetSymmetrySpec.js
+++ spec/AlphabetSymmetrySpec.js
@@ -4,4 +4,9 @@ describe("Solve", function() {
         expect(solve(["abc"])).toEqual(3);
         expect(solve(["abode"])).toEqual(4)
     })
+
+    it("solves for a single word containing upper and lower case letters", function() {
+        expect(solve(["aBc"])).toEqual(3);
+        expect(solve(["aBODe"])).toEqual(4)
+    })
 })
