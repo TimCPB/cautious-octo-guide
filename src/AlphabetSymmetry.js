4030a6032e2d0eaf115b1ea0eb50969f49fab8f0
Tim <timcpb@hotmail.com>
2020-11-05 14:00:47 +0000

    refactor with map function and extract to a private method

--- src/AlphabetSymmetry.js
+++ src/AlphabetSymmetry.js
@@ -1,21 +1,12 @@
-const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
 function solve(arr){
-    return arr.map(word => {
-        let count = 0
-        word.toLowerCase().split("").forEach((letter, index) => {
-        if (letter === alphabet[index]) count++
-        console.log(letter, index, alphabet[index])
-        });
-        return count
-    })
+    return arr.map( word => _howManyOrderedLetters(word) )
 }
 
-// function _howManyOrderedLetters(word){
-//     let count = 0
-//     word.toLowerCase().split("").forEach((letter, index) => {
-//         if (letter === alphabet[index]) count++
-//         console.log(letter, index, alphabet[index])
-//     });
-//     return count
-// }
+function _howManyOrderedLetters(string){
+    const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    let count = 0
+    string.toLowerCase().split("").forEach((letter, index) => {
+        if (letter === alphabet[index]) count++
+    });
+    return count
+}
