ec1c785df6d9edb7c064c34aad76d46b68ffa915
Tim <timcpb@hotmail.com>
2020-11-05 13:31:25 +0000

    green test - solves for a single, lowercase word

--- spec/AlphabetSymmetrySpec.js
+++ spec/AlphabetSymmetrySpec.js
@@ -1,6 +1,7 @@
 describe("Solve", function() {
 
     it("solves for a single, lowercase word", function() {
-        expect(solve(["abc"])).toEqual(3)
+        expect(solve(["abc"])).toEqual(3);
+        expect(solve(["abode"])).toEqual(4)
     })
 })
--- src/AlphabetSymmetry.js
+++ src/AlphabetSymmetry.js
@@ -1,3 +1,11 @@
+const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 function solve(arr){
-    
+    let word = arr[0]
+    let count = 0
+    word.split("").forEach((letter, index) => {
+        if (letter === alphabet[index]) count++
+        console.log(letter, index, alphabet[index])
+    });
+    return count
 }
