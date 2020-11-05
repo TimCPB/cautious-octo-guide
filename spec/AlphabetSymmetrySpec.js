c88002db9a1a75c2fa07862265fd3d8eee97ac3e
Tim <timcpb@hotmail.com>
2020-11-05 13:44:01 +0000

    green test - takes an array of words and returns an array of solutions

--- spec/AlphabetSymmetrySpec.js
+++ spec/AlphabetSymmetrySpec.js
@@ -1,13 +1,13 @@
 describe("Solve", function() {
 
     it("solves for a single, lowercase word", function() {
-        expect(solve(["abc"])).toEqual(3);
-        expect(solve(["abode"])).toEqual(4)
+        expect(solve(["abc"])).toEqual([3]);
+        expect(solve(["abode"])).toEqual([4])
     })
 
     it("solves for a single word containing upper and lower case letters", function() {
-        expect(solve(["aBc"])).toEqual(3);
-        expect(solve(["aBODe"])).toEqual(4)
+        expect(solve(["aBc"])).toEqual([3]);
+        expect(solve(["aBODe"])).toEqual([4])
     })
 
     it("takes an array of words and returns an array of solutions", function() {
--- src/AlphabetSymmetry.js
+++ src/AlphabetSymmetry.js
@@ -1,11 +1,21 @@
 const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 function solve(arr){
-    let word = arr[0]
-    let count = 0
-    word.toLowerCase().split("").forEach((letter, index) => {
+    return arr.map(word => {
+        let count = 0
+        word.toLowerCase().split("").forEach((letter, index) => {
         if (letter === alphabet[index]) count++
         console.log(letter, index, alphabet[index])
-    });
-    return count
+        });
+        return count
+    })
 }
+
+// function _howManyOrderedLetters(word){
+//     let count = 0
+//     word.toLowerCase().split("").forEach((letter, index) => {
+//         if (letter === alphabet[index]) count++
+//         console.log(letter, index, alphabet[index])
+//     });
+//     return count
+// }
