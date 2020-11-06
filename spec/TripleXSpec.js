c3aea006e8015f6bc1548f7e6e86962aafe08ab3
Tim <timcpb@hotmail.com>
2020-11-06 18:54:33 +0000

    red test - returns true for a single string of xxx

---
+++ spec/TripleXSpec.js
@@ -0,0 +1,6 @@
+describe ("tripleX", function(){
+
+    it("returns true for a single string of xxx", function() {
+        expect(tripleX("xxx")).toEqual(true)
+    })
+})
---
+++ src/TripleX.js
@@ -0,0 +1,3 @@
+function tripleX(str){
+    return str
+}
