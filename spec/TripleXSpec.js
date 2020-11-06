511a3a870439c7acc8380da88a932544472f07d8
Tim <timcpb@hotmail.com>
2020-11-06 18:58:27 +0000

    red test - only returns true if xxx is the first instance of x in a string

--- spec/TripleXSpec.js
+++ spec/TripleXSpec.js
@@ -3,4 +3,8 @@ describe ("tripleX", function(){
     it("returns true for a single string of xxx", function() {
         expect(tripleX("xxx")).toEqual(true)
     })
+
+    it("only returns true if xxx is the first instance of x in a string", function() {
+        expect(tripleX("softx kitty, warm kitty, xxxxx")).toEqual(false)
+    })
 })
