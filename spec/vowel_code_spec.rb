e34c6d1ac8eca63e436b474430a8a0bfeb9adeb5
Tim <timcpb@hotmail.com>
2020-11-12 17:29:18 +0000

    green test - replaces vowels with their respective numbers

--- spec/vowel_code_spec.rb
+++ spec/vowel_code_spec.rb
@@ -10,4 +10,11 @@ describe "encode" do
         expect(encode('a')).to eq('1')
     end
 
+    it "replaces vowels with their respective numbers" do
+        expect(encode('e')).to eq('2')
+        expect(encode('i')).to eq('3')
+        expect(encode('o')).to eq('4')
+        expect(encode('u')).to eq('5')
+    end
+
 end
--- src/vowel_code.rb
+++ src/vowel_code.rb
@@ -2,6 +2,14 @@ def encode(s)
 
     if s == 'a'
         "1"
+    elsif s == 'e'
+        '2'
+    elsif s == 'i'
+        '3'
+    elsif s == 'o'
+        '4'
+    elsif s == 'u'
+        '5'
     else
         s
     end
