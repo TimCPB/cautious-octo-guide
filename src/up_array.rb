1478a6ee1cbbafd48c4cc71c954e218d8d389922
Tim <timcpb@hotmail.com>
2020-11-13 18:12:13 +0000

    refactor

--- src/up_array.rb
+++ src/up_array.rb
@@ -1,9 +1,5 @@
 def up_array(arr)
-    arr.each do |x|
-        if !x.is_a?(Integer) || x < 0
-            return nil
-        end
-    end
+    arr.each { |x| return nil if !x.is_a?(Integer) || x < 0 }
     arr[(arr.length) - 1] = (arr.last) + 1
     arr
 end
