763993eaf8dc6a1a6f651e6910ca775884877087
Tim <timcpb@hotmail.com>
2020-11-14 00:43:31 +0000

    green test - returns the correct score for the different triples

--- spec/greed_is_good_spec.rb
+++ spec/greed_is_good_spec.rb
@@ -10,4 +10,13 @@ describe "Score" do
         expect(score([5])).to eq(50)
     end
 
+    it "returns the correct score for the different triples" do
+        expect(score([1, 1, 1, 3, 4])).to eq(1000)
+        expect(score([6, 6, 6, 3, 4])).to eq(600)
+        expect(score([5, 5, 5, 3, 4])).to eq(500)
+        expect(score([4, 4, 4, 2, 6])).to eq(400)
+        expect(score([3, 3, 3, 2, 6])).to eq(300)
+        expect(score([2, 2, 2, 3, 4])).to eq(200)
+    end
+
 end
--- src/greed_is_good.rb
+++ src/greed_is_good.rb
@@ -1,7 +1,46 @@
 def score( dice )
-    if dice[0] == 1
-        100
-    elsif dice[0] == 5
-        50
+    one_count = 0
+    two_count = 0
+    three_count = 0
+    four_count = 0
+    five_count = 0
+    six_count = 0
+    score = 0
+
+    dice.each do |x|
+        if x == 1
+            one_count += 1
+        elsif x == 2
+            two_count += 1
+        elsif x == 3
+            three_count += 1
+        elsif x == 4
+            four_count += 1
+        elsif x == 5
+            five_count += 1
+        elsif x == 6
+            six_count += 1
+        end
     end
+
+    if one_count == 1
+        score += 100
+    elsif five_count == 1
+        score += 50
+    elsif one_count == 3
+        score += 1000
+    elsif two_count == 3
+        score += 200
+    elsif three_count == 3
+        score += 300
+    elsif four_count == 3
+        score += 400
+    elsif five_count == 3
+        score += 500
+    elsif six_count == 3
+        score += 600
+    end
+
+    return score
+    
 end
