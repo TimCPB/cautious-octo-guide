8059302e8aeb5807610633c9d314b2b01dc5abd3
Tim <timcpb@hotmail.com>
2020-11-20 00:16:22 +0000

    green test - compares all elements in b and a regardless of order

--- src/comp.rb
+++ src/comp.rb
@@ -1,7 +1,10 @@
 def comp(array1, array2)
     result = true
-    array1.each.with_index do |num, index|
-        if num != Math.sqrt(array2[index])
+    array2.each do |num|
+        num_to_compare = Math.sqrt(num)
+        if array1.include?(num_to_compare)
+            array1.delete(num_to_compare)
+        else
             result = false
         end
     end
