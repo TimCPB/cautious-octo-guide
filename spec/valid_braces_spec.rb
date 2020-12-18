79146583a818c602c776e85f84a21db90739a417
Tim <timcpb@hotmail.com>
2020-12-18 13:26:40 +0000

    returns false for un-matching pairs of parentheses

--- spec/valid_braces_spec.rb
+++ spec/valid_braces_spec.rb
@@ -5,4 +5,16 @@ describe 'valid_braces' do
   it 'returns true for a pair of parenthesis' do
     expect(valid_braces("()")).to eq(true)
   end
+
+  it 'returns false for a pair of mismatched parenthesis' do
+    expect(valid_braces("((")).to eq(false)
+  end
+
+  it 'returns true for matching pairs of parentheses' do
+    expect(valid_braces("(())")).to eq(true)
+  end
+
+  it 'returns false for un-matching pairs of parentheses' do
+    expect(valid_braces("))((")).to eq(false)
+  end
 end
--- src/valid_braces.rb
+++ src/valid_braces.rb
@@ -1,5 +1,15 @@
 def valid_braces(braces)
-  if braces == "()"
-    true
+  braces_array = braces.chars
+  open_parentheses = 0
+  close_parentheses = 0
+  
+  braces_array.each do |brace|
+    if brace == "("
+      open_parentheses += 1
+    elsif brace == ")"
+      close_parentheses +=1
+    end
   end
+  
+  open_parentheses == close_parentheses && braces == braces.chars.sort.join("") ? true : false
 end
