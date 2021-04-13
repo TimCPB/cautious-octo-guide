2639fd605ebcf4f0b0e6eaa3cc42097014b66179
Tim <timcpb@hotmail.com>
2021-04-13 18:35:06 +0100

    add commented out test for nested braces

--- spec/valid_braces_spec.rb
+++ spec/valid_braces_spec.rb
@@ -1,32 +1,35 @@
-require '../src/valid_braces.rb'
+require '../src/valid_braces'
 
 describe 'valid_braces' do
-  
   it 'returns true for a pair of parenthesis' do
-    expect(valid_braces("()")).to eq(true)
+    expect(valid_braces('()')).to eq(true)
   end
 
   it 'returns false for a pair of mismatched parenthesis' do
-    expect(valid_braces("((")).to eq(false)
+    expect(valid_braces('((')).to eq(false)
   end
 
   it 'returns true for matching pairs of parentheses' do
-    expect(valid_braces("(())")).to eq(true)
+    expect(valid_braces('(())')).to eq(true)
   end
 
   it 'returns false for un-matching pairs of parentheses' do
-    expect(valid_braces("))((")).to eq(false)
+    expect(valid_braces('))((')).to eq(false)
   end
 
   it 'returns false for non-nested pairs of parentheses' do
-    expect(valid_braces("(")).to eq(false)
+    expect(valid_braces('(')).to eq(false)
   end
 
   it 'returns true for pairs of different braces' do
-    expect(valid_braces("(){}")).to eq(true)
+    expect(valid_braces('(){}')).to eq(true)
   end
 
   it 'returns false for non-nested pairs of different braces' do
-    expect(valid_braces("({)}")).to eq(false)
+    expect(valid_braces('({)}')).to eq(false)
   end
+
+  # it 'returns true for nested pairs of different braces' do
+  #   expect(valid_braces("({})")).to eq(true)
+  # end
 end
