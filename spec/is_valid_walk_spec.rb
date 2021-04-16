53cc275bc95282d60d30b1a1604183d6dcdb3778
Tim <timcpb@hotmail.com>
2021-04-16 17:30:54 +0100

    red test - 'returns false when given N and S'

--- spec/is_valid_walk_spec.rb
+++ spec/is_valid_walk_spec.rb
@@ -4,4 +4,8 @@ describe 'is_valid_walk.rb' do
   it "returns true when given a pair of 'N's" do
     expect(is_valid_walk(['n', 'n'])).to eq(true)
   end
+
+  it "returns false when given N and S" do
+    expect(is_valid_walk(['n', 'n'])).to eq(false)
+  end
 end
