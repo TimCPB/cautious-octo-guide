0c110df31517033a3a1704a486d8b30b32878773
Tim <timcpb@hotmail.com>
2021-04-16 23:00:34 +0100

    passing test - returns true for an array of 10 with matching elements

--- src/is_valid_walk.rb
+++ src/is_valid_walk.rb
@@ -1,3 +1,19 @@
 def is_valid_walk(walk)
-  true
+  return false if walk.length != 10
+  n_count = 0
+  s_count = 0
+  e_count = 0
+  w_count = 0
+  walk.each do |direction| 
+  if direction == 'n'
+    n_count += 1
+  elsif direction == 's'
+    s_count += 1
+  elsif direction == 'e'
+    e_count += 1
+  elsif direction == 'w'
+    w_count += 1
+  end
+end
+n_count == s_count && e_count == w_count ? true : false
 end
