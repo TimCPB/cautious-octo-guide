2b5ff4b36fe8d8f36f36cfb730be0e59152983d2
Tim <timcpb@hotmail.com>
2021-04-23 12:06:08 +0100

    red test - turns the array into a string

--- spec/is_valid_walk_spec.rb
+++ spec/is_valid_walk_spec.rb
@@ -1,14 +1,14 @@
-require '../src/is_valid_walk'
+# require '../src/is_valid_walk'
 
-describe 'is_valid_walk.rb' do
-  it "returns false when given a pair of 'N's" do
-    expect(is_valid_walk(['n', 'n'])).to eq(false)
-  end
+# describe 'is_valid_walk.rb' do
+#   it "returns false when given a pair of 'N's" do
+#     expect(is_valid_walk(['n', 'n'])).to eq(false)
+#   end
 
-  it "returns false when given a matching array of less than 10 elements" do
-    expect(is_valid_walk(['n', 's'])).to eq(false)
-  end
-  it "returns true for an array of 10 with matching elements" do
-    expect(is_valid_walk(['n','s','n','s','n','s','n','s','e','w'])).to eq(true)
-  end
-end
+#   it "returns false when given a matching array of less than 10 elements" do
+#     expect(is_valid_walk(['n', 's'])).to eq(false)
+#   end
+#   it "returns true for an array of 10 with matching elements" do
+#     expect(is_valid_walk(['n','s','n','s','n','s','n','s','e','w'])).to eq(true)
+#   end
+# end
